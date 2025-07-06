f4e28d0b3850992447eff5afb1b10690951cc979
JEREMIA1964 <bruder_joerg@hotmail.com>
2025-07-06 05:58:50 +0200

    feat: Python package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ez-chajim-meta",
+    version="5785.8.8",
+    author="JEREMIA1964",
+    author_email="bruder_joerg@hotmail.com",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
